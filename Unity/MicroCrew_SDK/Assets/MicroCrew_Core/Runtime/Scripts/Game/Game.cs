01732d5102e06fc05a64eef3ed2ea08b6abcba8f
beatenpixel <beaten.pixel.studio@gmail.com>
2021-10-24 22:21:40 +0300

    Added MicroCrew_Core

---
+++ Unity/MicroCrew_SDK/Assets/MicroCrew_Core/Runtime/Scripts/Game/Game.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MicroCrew.Core {
+
+	public class Game : MonoBehaviour {
+
+		public string gameName;
+
+	}
+
+}
