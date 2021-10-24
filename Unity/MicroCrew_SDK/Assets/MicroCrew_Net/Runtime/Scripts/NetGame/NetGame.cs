e6f3204a872c12dfb93908af0ef1db4f82138bae
beatenpixel <beaten.pixel.studio@gmail.com>
2021-10-24 23:04:29 +0300

    MicroCrew_Net

---
+++ Unity/MicroCrew_SDK/Assets/MicroCrew_Net/Runtime/Scripts/NetGame/NetGame.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MicroCrew.Net {
+
+	public class NetGame : MonoBehaviour {
+
+		public string gameName;
+
+	}
+
+}
